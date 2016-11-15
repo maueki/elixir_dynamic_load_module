d8b7ebf3334fff69e684d9d89ce8484e62d20ed0
Ueki Masaru <masaru.ueki@itage.co.jp>
2016-11-15 16:08:18 +0900

    pluginモジュールのdeps読み込み

--- parent/lib/parent.ex
+++ parent/lib/parent.ex
@@ -1,5 +1,10 @@
 defmodule Parent do
   def run() do
+
+    Mix.Project.in_project(:plugin, "../plugin", [], fn _ ->
+      Mix.Tasks.Deps.Loadpaths.run([])
+    end)
+
     Code.prepend_path("../plugin/_build/dev/lib/plugin/ebin")
 
     Code.ensure_loaded(Plugin)
